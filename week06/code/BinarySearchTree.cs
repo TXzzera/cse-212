0878ef94e0d358668c2e5695e994192575be5609
TXzzera <brunoteixeira.10@hotmail.com>
2026-04-15 23:36:53 -0300

    week06 att

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -79,10 +79,15 @@ public class BinarySearchTree : IEnumerable<int>
     }
 
     private void TraverseBackward(Node? node, List<int> values)
+{
+    // TODO Problem 3
+    if (node is not null)
     {
-        // TODO Problem 3
+        TraverseBackward(node.Right, values);
+        values.Add(node.Data);
+        TraverseBackward(node.Left, values);
     }
-
+}
     /// <summary>
     /// Get the height of the tree
     /// </summary>
